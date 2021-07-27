2798c20cd68a6dfa2f6dfd035ee7c5a5c2aff518
Nicolò Santamaria <nicolo.santamaria@protonmail.com>
2021-07-27 19:42:22 +0200

    Bug fix.

--- main.go
+++ main.go
@@ -201,7 +201,8 @@ func handlePut(w http.ResponseWriter, r *http.Request) {
 				continue
 			}
 
-			fdir := strings.TrimPrefix(r.URL.Path, "/put/")
+			fdir := strings.TrimPrefix(r.URL.Path, "/put")
+			fdir = strings.TrimPrefix(fdir, "/")
 			fpath := filepath.Join(cfg.BaseDir, fdir, fname)
 			wg.Add(1)
 			go func(fdir, fpath string, cnt []byte) {
@@ -537,6 +538,7 @@ func main() {
 	log.Println("setting the base directory at", cfg.BaseDir)
 	http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir(cfg.BaseDir))))
 	http.HandleFunc("/get", handleGet)
+	http.HandleFunc("/put", handlePut)
 	http.HandleFunc("/put/", handlePut)
 	http.HandleFunc("/del", handleDel)
 	http.HandleFunc("/del/", handleDel)
