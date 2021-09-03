879fad9d99df22ee2d018b43d68a33843a0e2445
Nicolò Santamaria <nicolo.santamaria@protonmail.com>
2021-09-03 11:28:12 +0200

    Update installation script.

--- install.sh
+++ install.sh
@@ -1,17 +1,55 @@
 #!/bin/sh
 
-if ! command go &> /dev/null
-then
-	echo "You need to have go to compile and install Adam."
-	exit
-fi
+function install {
+	if ! command -v go &> /dev/null
+	then
+		echo "You need to have go to compile and install Adam."
+		exit
+	fi
 
-echo 'Compiling...'
-go build
+	echo 'Compiling...'
+	go build
 
-echo 'Copying adam to /usr/bin...'
-sudo cp adam /usr/bin
+	echo 'Copying adam to /usr/bin...'
+	sudo cp adam /usr/bin
 
-echo 'Installing adam manual...'
-gzip -c adam.1 > adam.1.gz
-sudo cp adam.1.gx /usr/share/man/man1/
+	echo 'Installing adam manual...'
+	gzip -c adam.1 > adam.1.gz
+	sudo cp adam.1.gz /usr/share/man/man1/
+}
+
+function uninstall {
+	echo 'Removing /usr/bin/adam...'
+	sudo rm /usr/bin/adam
+
+	echo "Removing adam's manual"
+	sudo rm /usr/share/man/man1/adam.1.gz
+}
+
+function usage {
+	cat << EOF
+Usage: $0 OPTIONS
+
+This script installs or uninstalls Adam.
+
+OPTIONS:
+	-i            Install Adam.
+	-r            Uninstall Adam.
+	-h, --help    Print this help message.
+EOF
+}
+
+case $1 in
+	-h | --help)
+		usage
+		;;
+	-i)
+		install
+		;;
+	-r)
+		uninstall
+		;;
+	*)
+		usage
+		;;
+esac
